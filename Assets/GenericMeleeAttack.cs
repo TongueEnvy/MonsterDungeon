c64c6e6c44ccf3e69bd3ff5e66e3b414965b4ba5
chase cameron <chase cameron>
2018-05-19 21:25:01 -0700

    Correcting this merge error, hopefully.

--- Assets/GenericMeleeAttack.cs
+++ Assets/GenericMeleeAttack.cs
@@ -2,26 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenericMeleeAttack : MonoBehaviour {
-
+public class GenericMeleeAttack: MonoBehaviour {
     public bool canAttack;
     public Animator animator;
     public List<AnimationClip> attacks;
 
-    int currentAttack;
+    private int currentAttack = 0;
 
-    public void MeleeAttack()
-    {
-
+    public void MeleeAttack() {
         canAttack = false;
         animator.Play(attacks[currentAttack].name);
         currentAttack += 1;
-        if (currentAttack > (attacks.Count - 1))
-        {
-
+        
+		if (currentAttack > (attacks.Count - 1)) {
             currentAttack = 0;
-
         }
-
     }
 }
