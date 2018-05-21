60d6ba1617dc9e4a54dc56d6fc214cc4d62b9e6a
chase cameron <chase cameron>
2018-05-21 01:01:53 -0700

    Added a basic 5x5 generation (no connectivity) and got buttons to work. Need to get it to display though.

--- Assets/Scripts/MinimapGenerator.cs
+++ Assets/Scripts/MinimapGenerator.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinimapGenerator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class MinimapGenerator: MonoBehaviour {
+	public GameObject room;
+	public GameObject door;
+	public GameObject playerPosition;
+	
+	private bool[,] grid = new bool[5, 5];
+	
+	private void generateSpeckles() {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(Random.value >= 0.5) {
+					grid[i, j] = true;
+				}
+				
+				else {
+					grid[i, j] = false;
+				}
+			}
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void createMinimap() {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(grid[i, j] == true) {
+					Instantiate(room,
+								new Vector3((-30f * (float)i),
+											(-30f * (float)j),
+											0f),
+								transform.rotation);
+				}
+			}
+		}
+	}
+	
+	public void genMapButton() {
+		generateSpeckles();
+		createMinimap();
 	}
 }
