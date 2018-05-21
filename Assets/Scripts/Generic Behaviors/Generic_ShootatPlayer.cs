f2ede558acec628322bbe7aaa4723ba0c1139bc2
TongueEnvy <mr.dfelkins@gmail.com>
2018-05-20 21:53:53 -0700

    Added new Boss: Giga Berry Added new Enemy: Doll's Eye Berry

---
+++ Assets/Scripts/Generic Behaviors/Generic_ShootatPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic_ShootatPlayer : MonoBehaviour {
+
+    public string playerName;
+    GameObject player;
+    public GameObject bullet;
+    public float shotSpeed;
+    public float shotDelay;
+    float shotCounter;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find(playerName);
+        shotCounter = shotDelay;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        shotCounter -= Time.deltaTime;
+
+        if(shotCounter <= 0)
+        {
+
+            var newBullet = Instantiate<GameObject>(bullet);
+            newBullet.transform.localPosition = new Vector3(transform.position.x, .5f, transform.position.z);
+            newBullet.transform.LookAt (new Vector3(player.transform.position.x,.5f, player.transform.position.z));
+            newBullet.GetComponent<Script_ProjectileMove>().speed = shotSpeed;
+            shotCounter = shotDelay;
+
+        }
+
+	}
+}
---
+++ Assets/Scripts/Generic Behaviors/Generic_SpawnEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic_SpawnEnemy : MonoBehaviour {
+
+    public List<GameObject> possibleEnemies;
+    [HideInInspector] public GameObject spawnedEnemy;
+
+	public void SpawnEnemy()
+    {
+
+        var enemyToSpawn = (int)Random.Range(0, (possibleEnemies.Count));
+        spawnedEnemy = Instantiate<GameObject>(possibleEnemies[enemyToSpawn]);
+        spawnedEnemy.transform.parent = gameObject.transform;
+        spawnedEnemy.transform.localPosition = Vector3.zero;
+        spawnedEnemy.transform.parent = null;
+
+    }
+}
