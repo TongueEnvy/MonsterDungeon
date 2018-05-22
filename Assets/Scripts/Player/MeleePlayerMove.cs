568fce62112193f96d81b6d55764b39455497513
chase cameron <chase cameron>
2018-05-21 19:05:19 -0700

    File heirarchy minorly restructured. Created a new movement script.

--- Assets/Scripts/Player/MeleePlayerMove.cs
+++ Assets/Scripts/Player/MeleePlayerMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script_testMeleePlayerMove: MonoBehaviour {
+public class MeleePlayerMove: MonoBehaviour {
     public GameObject cursor;
     public GameObject model;
     public GameObject camAnchor;
