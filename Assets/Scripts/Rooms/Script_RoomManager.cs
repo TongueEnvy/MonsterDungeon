ccbd249c960f58e50d8a66e98586f959775f87f7
chase cameron <chase cameron>
2018-05-21 18:29:18 -0700

    Everything is refactored as much as I am willing to go. There was some organization already in place that is difficult to uproot without greater understanding.

--- Assets/Scripts/Rooms/Script_RoomManager.cs
+++ Assets/Scripts/Rooms/Script_RoomManager.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script_RoomManager : MonoBehaviour {
-
+public class Script_RoomManager: MonoBehaviour {
     public List<GameObject> enemies;
     public List<GameObject> spawners;
     public List<GameObject> doors;
@@ -11,51 +10,36 @@ public class Script_RoomManager : MonoBehaviour {
     public AnimationClip doorsClose;
 
     private void OnTriggerEnter(Collider other)
-    {
-
+	{
         if (other.gameObject.GetComponent<script_testMeleePlayerMove>())
         {
-
             other.gameObject.GetComponent<script_testMeleePlayerMove>().currentRoom = gameObject;
-
         }
 
         foreach(GameObject item in spawners)
         {
-
             item.gameObject.GetComponent<Generic_SpawnEnemy>().SpawnEnemy();
             item.gameObject.GetComponent<Generic_SpawnEnemy>().spawnedEnemy.gameObject.GetComponent<Script_AddEnemyToRoom>().currentRoom = gameObject;
             enemies.Add(item.gameObject.GetComponent<Generic_SpawnEnemy>().spawnedEnemy);
-
         }
 
         if(enemies.Count > 0)
         {
             foreach (GameObject item in doors)
             {
-
                 item.gameObject.GetComponent<Animator>().Play(doorsClose.name);
-
             }
         }
-        
-
     }
 
     private void Update()
     {
-        
         if(enemies.Count == 0)
         {
-
             foreach (GameObject item in doors)
             {
-
                 item.gameObject.GetComponent<Animator>().Play(doorsOpen.name);
-
             }
-
         }
-
     }
 }
